6a9d98e2d3e40e6b4e14099a481898611d55fd48
DragieZ <dragie.forumz@gmail.com>
2024-03-14 22:43:59 +0500

    Fix form.getHeaders Error for ePhoto Commands

--- XLICON-V3.js
+++ XLICON-V3.js
@@ -600,17 +600,17 @@ form.append("token", token)
 form.append("build_server", build_server)
 form.append("build_server_id", build_server_id)
 let res = await axios({
-  url: url,
-  method: "POST",
-  data: form,
-  headers: {
-    Accept: "*/*",
-    "Accept-Language": "en-US,en;q=0.9",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
-    cookie: gT.headers["set-cookie"]?.join("; "),
-    ...form.getHeaders()
-  }
-})
+    url: url,
+    method: "POST",
+    data: form,
+    headers: {
+      Accept: "*/*",
+      "Accept-Language": "en-US,en;q=0.9",
+      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+      cookie: gT.headers["set-cookie"]?.join("; "),
+      "Content-Type": "multipart/form-data"
+    }
+  });
 let $$ = cheerio.load(res.data)
 let json = JSON.parse($$("input[name=form_value_input]").val())
 json["text[]"] = json.text
