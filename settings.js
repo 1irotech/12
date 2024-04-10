57180964138b69eadd695f18c44af02f625a33b2
ray6531 <156494423+ray6531@users.noreply.github.com>
2024-04-10 22:27:33 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,19 +16,22 @@
 //                                                                                                      //
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //*
- //  * @project_name : REINER-MD
+ //  * @project_name : XLICON-V3-MD
  //  * @author : salmanytofficial
-//   * @description : REINER ,A Multi-functional whatsapp user bot.
+ //  * @youtube : https://www.youtube.com/@s4salmanyt
+//   * @description : XLICON-V3 ,A Multi-functional whatsapp user bot.
 //*
 //* 
 //base by DGXeon
 //re-upload? recode? copy code? give credit ya :)
 //Instagram: unicorn_xeon13
+//Telegram: t.me/ahmmitech
 //GitHub: @salmanytofficial
 //WhatsApp: +254742948136
-//   * Created By Github: REINER.
-//   * Credit To REINER
-//   * © 2024 RAY-MD.
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//   * Created By Github: DGXeon.
+//   * Credit To Xeon
+//   * © 2024 XLICON-V3-MD.
 // ⛥┌┤
 // */
 
@@ -43,16 +46,19 @@ global.ytname = process.env.YT_NAME || "YT: S4 Salman YT" //ur yt chanel name
 global.socialm = process.env.MEDIA || "GitHub: salmanytofficial" //ur github or insta name
 //________________________________________________________________________________________________________
 global.location = process.env.GL_INFO || "Pakistan, Lahore, Sabzazar" //ur location
-//_______________________________________________________________________________________________________________________________________________________________________________
-global.botname = process.env.BOT_NAME || 'RAY' //ur bot name
 //________________________________________________________________________________________________________
-global.ownernumber = process.env.SUDO || '254742948136//ur owner number
+
+//new
 //________________________________________________________________________________________________________
-global.ownername = process.env.SUDO_NAME || 'ray' //ur owner name
+global.botname = process.env.BOT_NAME || 'XLICON-V3-MD' //ur bot name
 //________________________________________________________________________________________________________
-global.websitex = process.env.YT_LINK || "https://youtube.almanyt"
+global.ownernumber = process.env.SUDO || '923184070915' //ur owner number
 //________________________________________________________________________________________________________
-global.wagc = process.env.GL_PUSH || "https://whatsapp.com/channel/0NgEZK0IBaK1nDtE3f"
+global.ownername = process.env.SUDO_NAME || 'Salman Ahmad' //ur owner name
+//________________________________________________________________________________________________________
+global.websitex = process.env.YT_LINK || "https://youtube.com/@s4salmanyt"
+//________________________________________________________________________________________________________
+global.wagc = process.env.GL_PUSH || "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f"
 //________________________________________________________________________________________________________
 global.themeemoji = process.env.GL_EMOJI || '🏮'
 //________________________________________________________________________________________________________
@@ -60,15 +66,15 @@ global.wm = process.env.GL_WM || "© Xʟɪᴄᴏɴ-Iɴᴄ"
 //________________________________________________________________________________________________________
 global.botscript = process.env.GL_SC || 'https://github.com/salmanytofficial/XLICON-V3-MD' //script link
 //________________________________________________________________________________________________________
-global.packname = process.env.PACK_NAME || "RAY-MD"
+global.packname = process.env.PACK_NAME || "𝐗𝐋𝐈𝐂𝐎𝐍-𝐕𝟑"
 //________________________________________________________________________________________________________
 global.author = process.env.AUTHER_NAME || "ᗰᗩᗪE ᗷY REINER"
 //________________________________________________________________________________________________________
 global.creator = process.env.GL_DEVS || "254742948136@s.whatsapp.net"
 //________________________________________________________________________________________________________
-global.xprefix = process.env.PREFIX || ','
+global.xprefix = process.env.PREFIX || '.'
 //________________________________________________________________________________________________________
-global.premium = process.env.GL_VIP || ["254742948136"] // Premium User
+global.premium = process.env.GL_VIP || ["923184070915"] // Premium User
 //________________________________________________________________________________________________________
 global.hituet = 0
 //________________________________________________________________________________________________________
@@ -83,7 +89,7 @@ global.autoblocknumber = process.env.AUTO_BAN || '212' //set autoblock country c
 //________________________________________________________________________________________________________
 global.antiforeignnumber = process.env.AUTO_BLOCK || '212' //set anti foreign number country code
 //________________________________________________________________________________________________________
-global.welcome = process.env.WELCOME_MSG || false //welcome/left in groups
+global.welcome = process.env.WELCOME_MSG || true //welcome/left in groups
 //________________________________________________________________________________________________________
 global.anticall = process.env.GL_NOCALL || false //bot blocks user when called
 //________________________________________________________________________________________________________
@@ -91,7 +97,7 @@ global.autoswview = process.env.AUTO_STATUS_SEEN || true //auto status/story vie
 //________________________________________________________________________________________________________
 global.adminevent = process.env.ANNOUNCE_MSG  || true //show promote/demote message
 //________________________________________________________________________________________________________
-global.groupevent = process.env.GROUP_ANNOUNCE_MSG || false //show update messages in group chat
+global.groupevent = process.env.GROUP_ANNOUNCE_MSG || true //show update messages in group chat
 //________________________________________________________________________________________________________
 
 
